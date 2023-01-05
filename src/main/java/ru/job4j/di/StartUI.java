1096b39bf6c1ea42591bee38a2cd9ac304a5ae43
kavalerov25 <paradise_777@mail.ru>
2023-01-05 23:18:37 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,10 +1,12 @@
 package ru.job4j.di;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 @Component
+@Scope("prototype")
 public class StartUI {
     @Autowired
     private Store store;
